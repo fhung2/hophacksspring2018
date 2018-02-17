b0fb9ae8e216b928b0661a0de9cee808a76e5bec
Joshua Russo <jrusso15@jhu.edu>
2018-02-17 11:25:48 -0500

    more results

--- gaussian_mix_sol.py
+++ gaussian_mix_sol.py
@@ -1,16 +1,16 @@
 from scipy.io import loadmat
-from sklearn.cluster import SpectralClustering
+from sklearn.mixture import GaussianMixture
 import numpy as np
 from sklearn.metrics.cluster import supervised
 from scipy.optimize import linear_sum_assignment
 
-data = loadmat('../digits/digits-train.mat')
+# data = loadmat('../digits/digits-train.mat')
 
-# data = loadmat('../objects/objects-train.mat')
+data = loadmat('../objects/objects-train.mat')
 
-#fea_hog = np.array(data['fea_hog_train'])
+fea_hog = np.array(data['fea_hog_train'])
 
-fea_hog = np.array(data['fea_scat_train'])
+# fea_hog = np.array(data['fea_scat_train'])
 
 labels = np.array(data['labels_train']).flatten()
 
@@ -19,15 +19,14 @@ print(labels.shape)
 print(fea_hog.transpose().shape)
 
 #test_gamma = [.5,.55,.6,.65,.7,.75,.8,.85,.9,.95,1]
-test_gamma = [.1,.15,.2,.25,.3,.35,.4,.45]
 #test = [1,2,3,4,5,6,7,8,9,10]
 
-#for x in test_gamma:
-spec_clust = SpectralClustering(n_clusters=5,affinity='nearest_neighbors',n_neighbors=3).fit(fea_hog.transpose())
+# for x in test:
+gauss_mix = GaussianMixture(n_components=5).fit(fea_hog.transpose())
 
 #print(spec_clust.get_params())
 
-labels_pred = np.array(spec_clust.labels_)
+labels_pred = np.array(gauss_mix.labels_)
 
 #print(labels_pred.shape)
 
@@ -38,8 +37,8 @@ value = supervised.contingency_matrix(labels, labels_pred)
 # value : array of shape [n, n] whose (i, j)-th entry is the number of samples in true class i and in predicted class j
 [r, c] = linear_sum_assignment(-value)
 accr = value[r, c].sum() / len(labels)
-print(accr)
-#print('gamma: ' + str(x) + ' accr: ' + str(accr))
+
+print('n_neighbors: ' + str(x) + ' accr: ' + str(accr))
 
 # for x in range(0,20):
 #     print(kmeans.labels_[x])
